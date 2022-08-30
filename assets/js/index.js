5f8aab179370adb07606d169e5ea38febb7afc7a
Erick Gamer56 <86386003+erickgamer56@users.noreply.github.com>
2022-08-30 09:05:01 -0300

    Salvo Na Ete

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,16 @@
 window.addEventListener("scroll" ,()=>{
-    var navbar = document.querySelector(".navbar")
+    var navbar = document.querySelectorAll(".navbar")
     var header = document.querySelector("header")
     var initofside = document.querySelector(".init_of_side")
     if (document.documentElement.scrollTop >= 1){
-       navbar.classList.add("fixed")
+      navbar.forEach(element => {
+         element.classList.add("fixed")
+      });
        header.classList.add("margin_navbar")
     }else{
-       navbar.classList.remove("fixed")
+      navbar.forEach(element => {
+         element.classList.remove("fixed")
+      });
        header.classList.remove("margin_navbar")
     }
 })
